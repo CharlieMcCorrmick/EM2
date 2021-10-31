bce9d250dad21f8cb1dcdcbb9e0d994d5fad33bb
Charlie McCorrmick <66713334+CharlieMcCorrmick@users.noreply.github.com>
2021-10-31 15:55:22 -0700

    Minor fix for launchability
    added some code to fix the EnergyScreen and fixed one final folder name

--- src/main/java/com/charlie/em2/screen/EnergyScreen.java
+++ src/main/java/com/charlie/em2/screen/EnergyScreen.java
@@ -1,15 +1,21 @@
 package com.charlie.em2.screen;
 
+import com.charlie.em2.EM2;
+import com.charlie.em2.container.EnergyContainer;
+import com.charlie.em2.container.QuarryOneContainer;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import net.minecraft.client.gui.screen.inventory.ContainerScreen;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.container.Container;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
 
-public class EnergyScreen extends ContainerScreen
+public class EnergyScreen extends ContainerScreen<EnergyContainer>
 {
+    private final ResourceLocation GUI = new ResourceLocation(EM2.MOD_ID,
+            "textures/gui/quarry_one_gui.png");
 
-    public EnergyScreen(Container screenContainer, PlayerInventory inv, ITextComponent titleIn)
+    public EnergyScreen(EnergyContainer screenContainer, PlayerInventory inv, ITextComponent titleIn)
     {
         super(screenContainer, inv, titleIn);
     }
